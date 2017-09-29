5ea885719ddc50fb2ea63cd0cb49aff9ac03d9f8
faisalpepe <32369236+faisalpepe@users.noreply.github.com>
2017-09-29 15:42:22 +0700

    Update vodkabot.py

--- vodkabot.py
+++ vodkabot.py
@@ -219,7 +219,7 @@ def SEND_MESSAGE(op):
                 if msg.text == "gift":
                     sendMessage(msg.to, text="gift sent", contentMetadata=None, contentType=9)
                 if msg.text == "P":
-                    sendMessage(msg.to, "ELt \n「tes」ER")
+                    sendMessage(msg.to, "ELER")
                     try:
                         del wait['readPoint'][msg.to]
                         del wait['readMember'][msg.to]
